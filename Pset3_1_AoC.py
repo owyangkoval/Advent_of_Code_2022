671c0ad821b3eb6e31e41d6236ee30a3fd221d35
owyangkoval <angela.owyang@gmail.com>
2023-01-03 12:10:55 -0800

    advent of code 2022

--- Pset3_1_AoC.py
+++ Pset3_1_AoC.py
@@ -4,19 +4,39 @@ def main():
     file = open('Pset3_1_AoC_input.txt', 'r')
     read = file.readlines()
 
+    sum = 0
+    for string in read:
+        for val in priorities_list(string):
+            sum += val
+    print(f"Sum of Priorities = {sum}")
 
-def split_string(string):
-    idx = 0
+def priorities_list(string):
+
+    # alphabet dict
+    alpha_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11,
+        'l': 12, 'm': 13,'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23,
+        'x': 24, 'y': 25, 'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35,
+        'J': 36, 'K': 37, 'L': 38, 'M': 39,'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47,
+        'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
+
+    # split string
+    idx = None
     first_half = []
     second_half = []
-    if idx <= (len(string))//2:
+    for idx in range(len(string)//2):
         first_half.append(string[idx])
-    if idx > (len(string))//2:
+    for idx in range(len(string)//2, len(string)):
         second_half.append(string[idx])
-    return first_half, second_half
 
-
-
+    # check item
+    priority_list = []
+    for letter_fh in first_half:
+        for letter_sh in second_half:
+            if letter_fh == letter_sh:
+                key = letter_fh
+                if key in alpha_dict and alpha_dict[key] not in priority_list:
+                    priority_list.append(alpha_dict[key])
+    return priority_list
 
 if __name__ == '__main__':
     main()
--- Pset3_1_AoC_unittest.py
+++ Pset3_1_AoC_unittest.py
@@ -2,19 +2,19 @@ import unittest
 
 # AoC Pset 3
 
-from Pset3_1_AoC import split_string
+from Pset3_1_AoC import priorities_list
 
 class TestString(unittest.TestCase):
 
-    def test_split_string(self):
+    def test_priorities_list(self):
         # Setup
         string = "MVWpzTTrTFNNLtssjV"
-        first_half, second_half = split_string(string)
+        first_half, second_half, priority_list = priorities_list(string)
 
         # Assert
-        self.assertEqual(first_half, "MVWpzTTrT")
-        self.assertEqual(second_half, "FNNLtssjV")
-
+        self.assertEqual(first_half, ['M', 'V', 'W', 'p', 'z', 'T', 'T', 'r', 'T'])
+        self.assertEqual(second_half, ['F', 'N', 'N', 'L', 't', 's', 's', 'j', 'V'])
+        self.assertEqual(priority_list, [48])
 
 if __name__ == '__main__':
     unittest.main()
